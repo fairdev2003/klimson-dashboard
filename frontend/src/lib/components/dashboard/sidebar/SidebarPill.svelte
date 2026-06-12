c60f383d49e645c5203d188e979602b53a31f0c4
fairdev2003 <jakubklimkiewicz70@gmail.com>
2026-06-12 19:29:19 +0200

    Changed 'Main page' to 'Hub', re-designed sidebar records and added customizing top-level pill in settings

---
+++ frontend/src/lib/components/dashboard/sidebar/SidebarPill.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { dashboard_config } from '$lib/dashboard/stores/persist';
+	import { nickname } from '../settings/store.svelte';
+	import SidebarUserLogged from '../SidebarUserLogged.svelte';
+	import StoragePill from './StoragePill.svelte';
+</script>
+
+{#if $dashboard_config.sidebar_preference === 'profile'}
+	<SidebarUserLogged
+		name={$nickname}
+		role="$root"
+		pfp_logo="https://klimson.dev/_app/immutable/assets/klimson.CQA0gh-5.jpeg"
+	/>
+{/if}
+
+{#if $dashboard_config.sidebar_preference === 'storage'}
+	<StoragePill />
+{/if}
---
+++ frontend/src/lib/components/dashboard/sidebar/StoragePill.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { api } from '$lib/api/api';
+	import { base_url } from '$lib/api/api.store';
+	import type { DiskData } from '$lib/api/requests/misc';
+	import { debug } from '$lib/dashboard/stores/debug';
+	import Icon from '@iconify/svelte';
+	import { onMount } from 'svelte';
+
+	let percent: number = $state(0);
+	let power = $derived(Math.floor(percent * 10));
+	let disk: DiskData | undefined = $state();
+	let usedGb = $derived(formatBytes(disk?.used ?? 0));
+	let totalGb = $derived(formatBytes(disk?.total ?? 0));
+	let loadingRoute = $state(false);
+
+	onMount(async () => {
+		const response = await api.misc.GetDisk();
+		disk = response.data;
+		percent = Number(disk.percentage) / 100;
+		debug.log('Procent z API: ' + disk.percentage);
+		debug.log('Wartość power: ' + power);
+
+		gsap.to('.blob-item', {
+			backgroundColor: (i) => (i < power ? '#22c55e' : '#525252'),
+			duration: 1,
+			stagger: 0.1,
+			ease: 'power1.inOut'
+		});
+	});
+
+	function formatBytes(bytes: number | string, decimals = 2) {
+		const b = Number(bytes);
+		if (b === 0) return '0 GB';
+
+		const gb = b / (1024 * 1024 * 1024);
+
+		return `${gb.toFixed(decimals)} GB`;
+	}
+</script>
+
+<div
+	class="bg-neutral-800 rounded-lg gap-2 flex flex-col w-full p-3 hover:bg-neutral-700 transition-colors cursor-pointer"
+>
+	<div class="flex gap-2 items-center">
+		<Icon icon="entypo:drive" width="17" height="17" />
+		<p class="text-sm font-black">
+			{$base_url === 'https://api.klimson.dev' ? 'Mikrus Storage' : 'Local Storage'}
+		</p>
+	</div>
+	<div class="w-full h-0.5 bg-neutral-500">
+		<div style="width: {Number(percent) * 100}%;" class="h-0.5 bg-green-500"></div>
+	</div>
+	<div>
+		<p class="text-xs text-neutral-400">
+			{usedGb} / {totalGb}
+		</p>
+	</div>
+</div>
