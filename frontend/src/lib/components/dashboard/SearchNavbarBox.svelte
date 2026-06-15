5aa1ece00b2051834db980fc75b3b1ff96c9474b
fairdev2003 <jakubklimkiewicz70@gmail.com>
2026-06-15 20:06:56 +0200

    Added bookmarks to search box

--- frontend/src/lib/components/dashboard/SearchNavbarBox.svelte
+++ frontend/src/lib/components/dashboard/SearchNavbarBox.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { dashboard_config } from '$lib/dashboard/stores/persist';
 	import Icon from '@iconify/svelte';
 	import { fade } from 'svelte/transition';
 	let value: string = $state('');
@@ -78,6 +79,14 @@
 		value === '' ? routes : routes.filter((e) => e.name.toLowerCase().includes(value.toLowerCase()))
 	);
 
+	const searchedBookmarks = $derived(
+		value === ''
+			? $dashboard_config.bookmarks
+			: $dashboard_config.bookmarks.filter((e) =>
+					e.name.toLowerCase().includes(value.toLowerCase())
+				)
+	);
+
 	$effect(() => {
 		if (opened) {
 			inputContainerRef?.focus();
@@ -96,6 +105,8 @@
 	/>
 
 	{#if opened}
+		<!-- svelte-ignore a11y_click_events_have_key_events -->
+		<!-- svelte-ignore a11y_no_static_element_interactions -->
 		<div
 			class="bg-black/50 fixed inset-0 z-90"
 			onclick={() => {
@@ -103,6 +114,8 @@
 			}}
 		></div>
 
+		<!-- svelte-ignore a11y_click_events_have_key_events -->
+		<!-- svelte-ignore a11y_no_static_element_interactions -->
 		<div
 			in:fade={{ duration: 300 }}
 			out:fade={{ duration: 300 }}
@@ -117,27 +130,9 @@
 			/>
 			<div class="mt-3 h-[300px] overflow-auto">
 				<div class="flex gap-3 flex-col">
-					{#if searchedRoutes.length > 0}
-						<p class="font-black">Dashboard routes</p>
-						{#each searchedRoutes as search_route}
-							<button
-								onclick={() => {
-									goto(search_route.url);
-									opened = false;
-								}}
-								class="flex items-center gap-3 w-full h-15 rounded-lg cursor-pointer bg-neutral-800 hover:bg-neutral-700 px-4 transition-colors"
-							>
-								<div
-									class="flex items-center justify-center size-8 bg-neutral-900 rounded-md shrink-0"
-								>
-									<Icon icon={search_route.icon} width="20" height="20" class="text-neutral-300" />
-								</div>
-								<div class="flex flex-col">
-									<p class="font-black text-start text-neutral-200">{search_route.name}</p>
-									<p class="text-xs text-start text-neutral-400">{search_route.description}</p>
-								</div>
-							</button>
-						{/each}
+					{#if searchedRoutes.length > 0 || searchedBookmarks.length > 0}
+						{@render RoutesTab()}
+						{@render BookmarksTab()}
 					{:else}
 						<p class="text-neutral-500 p-3">No results found</p>
 					{/if}
@@ -146,6 +141,49 @@
 		</div>
 	{/if}
 </div>
+
+{#snippet RoutesTab()}
+	<p class="font-black">Dashboard routes</p>
+	{#each searchedRoutes as search_route}
+		<button
+			onclick={() => {
+				goto(search_route.url);
+				opened = false;
+			}}
+			class="flex items-center gap-3 w-full h-15 rounded-lg cursor-pointer bg-neutral-800 hover:bg-neutral-700 px-4 transition-colors"
+		>
+			<div class="flex items-center justify-center size-8 bg-neutral-900 rounded-md shrink-0">
+				<Icon icon={search_route.icon} width="20" height="20" class="text-neutral-300" />
+			</div>
+			<div class="flex flex-col">
+				<p class="font-black text-start text-neutral-200">{search_route.name}</p>
+				<p class="text-xs text-start text-neutral-400">{search_route.description}</p>
+			</div>
+		</button>
+	{/each}
+{/snippet}
+
+{#snippet BookmarksTab()}
+	<p class="font-black">Saved Bookmarks</p>
+	{#each searchedBookmarks as search_bookmark}
+		<button
+			onclick={() => {
+				window.open(search_bookmark.href, '_blank', 'noopener,noreferrer');
+
+				opened = false;
+			}}
+			class="flex items-center gap-3 w-full h-15 rounded-lg cursor-pointer bg-neutral-800 hover:bg-neutral-700 px-4 transition-colors"
+		>
+			<div
+				class="flex items-center justify-center size-8 {search_bookmark.color} rounded-md shrink-0"
+			></div>
+			<div class="flex flex-col">
+				<p class="font-black text-start text-neutral-200">{search_bookmark.name}</p>
+				<p class="text-xs text-start text-neutral-400">{search_bookmark.slug}</p>
+			</div>
+		</button>
+	{/each}
+{/snippet}
 
 <svelte:document
 	onkeydown={(e) => {
