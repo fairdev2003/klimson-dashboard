aa5b5a356c7fea621d2cc2f742cd2b3d558e34da
fairdev2003 <jakubklimkiewicz70@gmail.com>
2026-06-28 19:34:05 +0200

    Added padding to mod list

--- frontend/src/routes/mod/list/+page.svelte
+++ frontend/src/routes/mod/list/+page.svelte
@@ -2,6 +2,6 @@
 	import MarkdownRenderer from '$lib/components/MarkdownRenderer.svelte';
 </script>
 
-<div class="lg:max-w-4xl flex mx-auto">
+<div class="lg:max-w-4xl flex mx-auto p-5">
 	<MarkdownRenderer markdown="https://api.klimson.dev/interface/bucket/klimson.dev/mod.list.md" />
 </div>
