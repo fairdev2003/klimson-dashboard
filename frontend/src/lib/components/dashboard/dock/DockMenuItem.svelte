9c8b317f57c346817240077ce4007484385b4c6d
fairdev2003 <jakubklimkiewicz70@gmail.com>
2026-07-31 11:55:29 +0200

    Sidebar pills added into the mobiledock, tweaked navigation between routes in mobiledock also and added some accessability things on mobile devices

---
+++ frontend/src/lib/components/dashboard/dock/DockMenuItem.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import { onMount, type Snippet } from 'svelte';
+	import { slide } from 'svelte/transition';
+
+	type Props = { opened: boolean; children: Snippet; name: string };
+
+	let { opened = $bindable(), children, name }: Props = $props();
+
+	onMount(() => {});
+</script>
+
+<div transition:slide={{ duration: 300 }} class="">
+	<div
+		onclick={() => {
+			opened = !opened;
+		}}
+		class="flex gap-2 items-center cursor-pointer select-none"
+	>
+		<div class="text-neutral-400 transition-transform duration-300 {opened ? 'rotate-180' : ''}">
+			<Icon icon="mdi:chevron-down" width="25" height="25" />
+		</div>
+
+		<h3 class="text-white font-bold capitalize text-lg">{name}</h3>
+	</div>
+	{#if opened}
+		<div class="">
+			{@render children()}
+		</div>
+	{/if}
+</div>
+
+<style>
+	@import 'tailwindcss';
+
+	.selector {
+		@apply;
+	}
+</style>
