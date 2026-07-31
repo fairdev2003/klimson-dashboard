b7ad5d8178dbf31bb4268a5401054329cbd6f3c8
fairdev2003 <jakubklimkiewicz70@gmail.com>
2026-07-31 12:46:25 +0200

    Added togglin sidebar

--- frontend/src/lib/components/dashboard/sidebar/SidebarToggler.svelte
+++ frontend/src/lib/components/dashboard/sidebar/SidebarToggler.svelte
@@ -11,6 +11,8 @@
 	onclick={() => {
 		onclick();
 	}}
+	class:selected-toggler={opened}
+	class:base-toggler={!opened}
 	class="p-2 cursor-pointer hover:bg-neutral-700 rounded-xl mb-4"
 >
 	<Icon icon="tabler:layout-sidebar" width="25" height="25"></Icon>
@@ -19,7 +21,11 @@
 <style>
 	@import 'tailwindcss';
 
-	.selector {
-		@apply;
+	.selected-toggler {
+		@apply bg-blue-700;
+	}
+
+	.base-toggler {
+		@apply bg-transparent hover:bg-neutral-700;
 	}
 </style>
