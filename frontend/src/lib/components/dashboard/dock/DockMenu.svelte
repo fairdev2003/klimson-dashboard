c0189663b8bdbe229106717e0f9a67f383465bde
fairdev2003 <jakubklimkiewicz70@gmail.com>
2026-07-31 11:28:46 +0200

    Added new animation to menu dock opening

--- frontend/src/lib/components/dashboard/dock/DockMenu.svelte
+++ frontend/src/lib/components/dashboard/dock/DockMenu.svelte
@@ -3,11 +3,25 @@
 	import Button from '$lib/components/Button.svelte';
 	import Dashboard from '$lib/dashboard/dashboard.svelte';
 	import { onMount } from 'svelte';
-	import { slide } from 'svelte/transition';
+	import { cubicOut } from 'svelte/easing';
+	import { fly, slide } from 'svelte/transition';
 
 	type Props = { mobileDockOpened: boolean };
 
 	let { mobileDockOpened = $bindable() }: Props = $props();
+
+	function whizz(node: HTMLElement, { from, to }: { from: DOMRect; to: DOMRect }, params: any) {
+		const dx = from.left - to.left;
+		const dy = from.top - to.top;
+
+		const d = Math.sqrt(dx * dx + dy * dy);
+
+		return {
+			delay: 0,
+			duration: Math.sqrt(d) * 120,
+			easing: cubicOut
+		};
+	}
 </script>
 
 <!-- svelte-ignore a11y_click_events_have_key_events -->
@@ -16,8 +30,8 @@
 	onclick={(e) => {
 		e.stopPropagation();
 	}}
-	transition:slide={{ duration: 300 }}
-	class="lg:hidden p-4 flex h-150 shadow-2xl w-full bg-neutral-900 absolute z-100 border-b border-neutral-800"
+	transition:fly={{ y: -40, duration: 250, easing: cubicOut }}
+	class="lg:hidden p-4 flex h-150 shadow-2xl w-full bg-neutral-900 absolute z-20 border-b border-neutral-800"
 >
 	<div class="flex flex-col gap-2 w-full">
 		{#each Dashboard.constants.SidebarContents as item}
