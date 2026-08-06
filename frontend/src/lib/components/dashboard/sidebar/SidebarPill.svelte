44efe4041ec1f174a448e76e30c8e9e3faa93e78
fairdev2003 <jakubklimkiewicz70@gmail.com>
2026-08-06 19:00:50 +0200

    Sidebar pills (user pills) added back

--- frontend/src/lib/components/dashboard/sidebar/SidebarPill.svelte
+++ frontend/src/lib/components/dashboard/sidebar/SidebarPill.svelte
@@ -5,4 +5,19 @@
 	import StoragePill from './StoragePill.svelte';
 </script>
 
-<div class="flex flex-col gap-3 -z-1"></div>
+<div class="flex flex-col gap-3 -z-1">
+	{#if $dashboard_config.client_pills}
+		{#if $dashboard_config.client_pills.includes('profile')}
+			<SidebarUserLogged
+				selected={false}
+				name={$nickname}
+				role="$root"
+				pfp_logo="https://klimson.dev/_app/immutable/assets/klimson.CQA0gh-5.jpeg"
+			/>
+		{/if}
+
+		{#if $dashboard_config.client_pills.includes('storage')}
+			<StoragePill />
+		{/if}
+	{/if}
+</div>
